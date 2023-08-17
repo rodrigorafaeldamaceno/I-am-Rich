f79669627fbba100a2bff6ce099ab519f3dd5a45
rodrigorafaeldamaceno <rodrigorafaeldamaceno@gmail.com>
2023-08-17 10:44:04 -0300

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by eMotion Studios on 17/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
